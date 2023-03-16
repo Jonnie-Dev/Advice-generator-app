94824ae3921a66c998f80cb7c548cb9d53ffe144
Jonnie-Dev <jtech2096@gmail.com>
2023-03-16 23:30:09 +0100

    feat: created quotes component

--- src/component/Quotes.jsx
+++ src/component/Quotes.jsx
@@ -1,13 +1,27 @@
+import React, { useState } from "react";
+
 import dividerLarge from "../assets/pattern-divider-desktop.svg";
 import dividerSmall from "../assets/pattern-divider-mobile.svg";
+import diceButton from "../assets/icon-dice.svg";
 
 const Quotes = () => {
+  const [quote, setQuote] = useState(null);
+  const [media, setMedia] = useState("(min-width:720px)");
+  // https://api.adviceslip.com
+  const handleClick = async () => {};
+
   return (
     // Template
-    <div>
+    <div className="bg-[#323a49]">
       <p>#Advice 12</p>
-      <h1>Bala Blue</h1>
-      <img src={dividerLarge} />
+      <p>
+        <q>Bala Blue</q>
+      </p>
+
+      <img src={dividerSmall} />
+      <div className="">
+        <img className="" src={diceButton} />
+      </div>
     </div>
   );
 };
