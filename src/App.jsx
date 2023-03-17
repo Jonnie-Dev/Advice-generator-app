05d609bdf74ffe6f42786cd1fa531595f5fa8b50
Jonnie-Dev <jtech2096@gmail.com>
2023-03-17 13:56:52 +0100

    feat: all ui elements fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import Quotes from "./component/Quotes";
 
 const App = () => {
   return (
     <div className="h-[100vh] flex items-center justify-center bg-deepBlue text-2xl">
-      <Quotes />
+      <div className="max-w-lg flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
+        <h1 className="text-neonGreen text-[.625rem] tracking-[4.25px]">
+          #ADVICE {"117"}
+        </h1>
+        <p className="text-cyan">
+          <q>
+            Content goes here as this is an example of a quote we will be
+            expectiong of a quote gubberish sha you get what im saying dont you
+          </q>
+        </p>
+        <img className="mb-4" src="/images/pattern-divider-mobile.svg" />
+        <div className="cursor-pointer  absolute top-[100%] right-[50%] translate-x-2/4 -translate-y-2/4 rounded-full p-4 bg-neonGreen shadow-lg shadow-cyan-500">
+          <img src="/images/icon-dice.svg" />
+        </div>
+      </div>
     </div>
   );
 };
