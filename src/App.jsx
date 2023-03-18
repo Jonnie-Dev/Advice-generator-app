7f3e6470f4a14be70ee9831c7ea5cde372df01a3
Jonnie-Dev <jtech2096@gmail.com>
2023-03-18 14:34:46 +0100

    fix: fixed all bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,58 @@
-
 import React, { useState } from "react";
-import { useMediaQuery } from "@mui/material";
-const isMobile = useMediaQuery("(max-width: 720px)");
 
-
-const App = () => {
-  const [qoutes, setQuote] = useState();
-  const url = "https://api.adviceslip.com/advice";
-  const handleClick = async () => {
+const url = "https://api.adviceslip.com/advice";
+const fetchData = async (url) => {
+  try {
     const res = await fetch(url);
     const data = await res.json();
-    setQuote(data.slip);
+    console.log(data);
+    return data;
+  } catch (error) {
+    let data = {
+      slip: {
+        id: 0,
+        advice: "Never give up, Keep trying.",
+      },
+    };
+    return data;
+  }
+};
+
+const App = () => {
+  const [qoutes, setQuote] = useState(0);
+
+  const handleClick = () => {
+    fetchData(url).then((data) => setQuote(data.slip));
   };
 
   return (
-
     <div className="h-[100vh] flex items-center justify-center bg-deepBlue text-2xl">
-      <div className="max-w-xl min-w-[300px] flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
+      <div className="max-w-lg mx-4 md:mx-0 flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
         <h1 className="text-neonGreen text-[.625rem] tracking-[4.25px]">
           #ADVICE {qoutes.id ? qoutes.id : 0}
         </h1>
+
         <p className="text-cyan">
           <q>{qoutes.advice}</q>
         </p>
-        <img
-          className="mb-4"
-          src={
-            isMobile
-              ? "/images/pattern-divider-mobile.svg"
-              : "/images/pattern-divider-desktop.svg"
-          }
-        />
+
+        <picture className="mb-6">
+          <source
+            srcSet="/images/pattern-divider-desktop.svg"
+            media="(min-width: 512px)"
+          />
+          <img src="/images/pattern-divider-mobile.svg" alt="divider image" />
+        </picture>
+
         <div
           onClick={handleClick}
           className="cursor-pointer transition-all absolute top-[100%] right-[50%] translate-x-2/4 -translate-y-2/4 rounded-full p-4 bg-neonGreen hover:shadow-[0px_4px_8px_4px_#52ffa833] active:scale-105"
         >
-
-    <div className=" bg-deepBlue flex max-w-lg">
-      <div className="items-center">
-        <h1 className="">#Advice 117 </h1>
-        <p>
-          <q>Content goes here</q>
-        </p>
-        <img src="/images/pattern-divider-mobile.svg" />
-        <div>
           <img src="/images/icon-dice.svg" />
         </div>
       </div>
     </div>
   );
 };
+
 export default App;
