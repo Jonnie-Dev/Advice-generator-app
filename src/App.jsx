90163023f10d7fd5f087052364643ca26793a82e
Jonnie-Dev <jtech2096@gmail.com>
2023-03-18 09:18:49 +0100

    feat: imported mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
 
 import React, { useState } from "react";
+import { useMediaQuery } from "@mui/material";
+const isMobile = useMediaQuery("(max-width: 720px)");
 
 
 const App = () => {
-  const [qoutes, setQuote] = useState(0);
+  const [qoutes, setQuote] = useState();
   const url = "https://api.adviceslip.com/advice";
   const handleClick = async () => {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(qoutes);
     setQuote(data.slip);
   };
 
   return (
 
     <div className="h-[100vh] flex items-center justify-center bg-deepBlue text-2xl">
-      <div className="max-w-lg flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
+      <div className="max-w-xl min-w-[300px] flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
         <h1 className="text-neonGreen text-[.625rem] tracking-[4.25px]">
           #ADVICE {qoutes.id ? qoutes.id : 0}
         </h1>
         <p className="text-cyan">
           <q>{qoutes.advice}</q>
         </p>
-        <img className="mb-4" src="/images/pattern-divider-mobile.svg" />
+        <img
+          className="mb-4"
+          src={
+            isMobile
+              ? "/images/pattern-divider-mobile.svg"
+              : "/images/pattern-divider-desktop.svg"
+          }
+        />
         <div
           onClick={handleClick}
           className="cursor-pointer transition-all absolute top-[100%] right-[50%] translate-x-2/4 -translate-y-2/4 rounded-full p-4 bg-neonGreen hover:shadow-[0px_4px_8px_4px_#52ffa833] active:scale-105"
