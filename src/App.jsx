7d8d11ceb4bcf3002c8288cce1d1c72cc7fce450
fortune <omorodion.fortune@gmail.com>
2023-03-18 14:53:32 +0100

    conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-
 import React, { useState } from "react";
-
 
 const App = () => {
   const [qoutes, setQuote] = useState(0);
@@ -13,7 +11,6 @@ const App = () => {
   };
 
   return (
-
     <div className="h-[100vh] flex items-center justify-center bg-deepBlue text-2xl">
       <div className="max-w-lg flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
         <h1 className="text-neonGreen text-[.625rem] tracking-[4.25px]">
@@ -27,19 +24,22 @@ const App = () => {
           onClick={handleClick}
           className="cursor-pointer transition-all absolute top-[100%] right-[50%] translate-x-2/4 -translate-y-2/4 rounded-full p-4 bg-neonGreen hover:shadow-[0px_4px_8px_4px_#52ffa833] active:scale-105"
         >
-
-    <div className=" bg-deepBlue flex max-w-lg">
-      <div className="items-center">
-        <h1 className="">#Advice 117 </h1>
-        <p>
-          <q>Content goes here</q>
-        </p>
-        <img src="/images/pattern-divider-mobile.svg" />
-        <div>
-          <img src="/images/icon-dice.svg" />
+          <div className=" bg-deepBlue flex max-w-lg">
+            <div className="items-center">
+              <h1 className="">#Advice 117 </h1>
+              <p>
+                <q>Content goes here</q>
+              </p>
+              <img src="/images/pattern-divider-mobile.svg" />
+              <div>
+                <img src="/images/icon-dice.svg" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default App;
