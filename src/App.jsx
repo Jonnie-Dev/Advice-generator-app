315ed490ef38741406032f57b1eb1864c637707f
fortune <omorodion.fortune@gmail.com>
2023-03-18 18:27:06 +0100

    all fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-
 import React, { useState } from "react";
 
+const url = "https://api.adviceslip.com/advice";
+const fetchData = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    let data = {
+      slip: {
+        id: 0,
+        advice: "Never give up, Keep trying.",
+      },
+    };
+    return data;
+  }
+};
 
 const App = () => {
   const [qoutes, setQuote] = useState(0);
@@ -11,7 +27,7 @@ const App = () => {
 
   return (
     <div className="h-[100vh] flex items-center justify-center bg-deepBlue text-2xl">
-      <div className="max-w-xs md:max-w-lg mx-4 md:mx-0 flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
+      <div className="max-w-lg mx-4 md:mx-0 flex flex-col gap-8 bg-grayishBlue-d p-8 rounded-lg text-center relative">
         <h1 className="text-neonGreen text-[.625rem] tracking-[4.25px]">
           #ADVICE {qoutes.id ? qoutes.id : 0}
         </h1>
@@ -32,21 +48,11 @@ const App = () => {
           onClick={handleClick}
           className="cursor-pointer transition-all absolute top-[100%] right-[50%] translate-x-2/4 -translate-y-2/4 rounded-full p-4 bg-neonGreen hover:shadow-[0px_4px_8px_4px_#52ffa833] active:scale-105"
         >
-          <div className=" bg-deepBlue flex max-w-lg">
-            <div className="items-center">
-              <h1 className="">#Advice 117 </h1>
-              <p>
-                <q>Content goes here</q>
-              </p>
-              <img src="/images/pattern-divider-mobile.svg" />
-              <div>
-                <img src="/images/icon-dice.svg" />
-              </div>
-            </div>
-          </div>
+          <img src="/images/icon-dice.svg" />
         </div>
       </div>
     </div>
   );
 };
+
 export default App;
